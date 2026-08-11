7f744117e46f370a2d660c459665a02e9056da3d
Harsh Saini <harsh.saini13881@gmail.com>
2026-08-11 10:56:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Without Extra Space/merge-without-extra-space.cpp
+++ Difficulty: Medium/Merge Without Extra Space/merge-without-extra-space.cpp
@@ -1,31 +1,56 @@
 class Solution {
-  public:
-    void mergeArrays(vector<int>& a, vector<int>& b) {
-        
-        int n = a.size();
-        int m = b.size();
-        vector<int> temp;
-        for(int i = 0 ; i < n ; i++){
-            temp.push_back(a[i]);
-            
-        }
-        for(int i = 0 ; i < m ; i++){
-            temp.push_back(b[i]);
-            
-        }
-        sort(temp.begin() , temp.end());
-        a.clear();
-        b.clear();
-        
-        int k = 0;
-        for(int i = 0 ; i < n ; i++){
-            a.push_back(temp[k]);
-            k++;
-        }
-        for(int i = 0 ; i < m ; i++){
-            b.push_back(temp[k]);
-            k++;
-        }
-        temp.clear();    
-    }
+	public:
+	void mergeArrays(vector<int>& a, vector<int>& b) {
+		int n = a.size();
+		int m = b.size();
+		
+		int total = n + m ;
+		int gap = (total + 1)/2;
+		
+		while (gap > 0) {
+			int i = 0;
+			int j = gap;
+			
+			while (j < total) {
+				
+				// First Case ---> dono element a me hi hai
+				
+				if (i < n && j < n) {
+					if (a[i] > a[j]) {
+						swap(a[i], a[j]);
+					}
+					
+				}
+				
+				/* Second Case ---> one element a me and another
+				element b me ho */
+				
+				else if (i < n && j >= n) {
+					if (a[i] > b[j - n]) {
+						swap(a[i], b[j - n]);
+					}
+				}
+				
+				// third Case ---> dono element b me hi hai
+				
+				else {
+					if (b[i - n] > b[j - n]) {
+						swap(b[i - n], b[j - n]);
+					}
+					
+				}
+				i++;
+				j++;
+				
+			}
+		
+    		if (gap == 1) {
+    			gap = 0;
+    		}
+    		else {
+    			gap = (gap + 1)/2;
+    		}
+		}
+		
+	}
 };
