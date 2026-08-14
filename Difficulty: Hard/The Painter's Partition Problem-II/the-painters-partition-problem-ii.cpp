49bd410d5269623f14b326f67ac04a31b6b3ca38
Harsh Saini <harsh.saini13881@gmail.com>
2026-08-14 17:14:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
@@ -1,52 +1,50 @@
 class Solution {
   public:
-    
     bool Is_possible(vector<int>& arr, int k ,long long mid){
-        int count = 0;
-        int painter = 1;
-        for (int i = 0 ; i < arr.size() ; i++){
-            if(count + arr[i] <= mid){
-                count += arr[i];
+        int Painter_Count = 1;
+        int board = 0;
+        for(int i = 0 ; i < arr.size() ; i++){
+            if(board + arr[i] <= mid){
+                board += arr[i];
             }
             else{
-                painter++;
-                if(painter > k || mid < arr[i]){
+                Painter_Count++;
+                if(k < Painter_Count || mid < arr[i]){
                     return false;
                 }
-                count = arr[i];
+                board = arr[i];
             }
         }
         return true;
-    }
-  
-  
+        
+    } 
     int Binary(vector<int>& arr, int k){
-        int start =  0 ;
-        long long sum = 0 ; 
-        for (int i = 0 ; i < arr.size() ; i++){
+        int start = 0 ;
+        long long sum  = 0;
+        for(int i = 0 ; i < arr.size() ; i++){
             sum += arr[i];
         }
-        long long end = sum ;
-        int ans = -1;
+        long long end = sum;
         long long mid = start + (end - start)/2;
+        int ans = -1;
+        if(k == 1){
+            return end;
+        }
         while(start <= end){
-            if (Is_possible(arr , k , mid)){
+            if(Is_possible(arr , k , mid)){
                 ans = mid;
                 end = mid - 1;
             }
             else{
                 start = mid + 1;
-                
             }
             mid = start + (end - start)/2;
         }
         return ans;
         
-        
     }
-  
     int minTime(vector<int>& arr, int k) {
-        if(k > arr.size()){
+        if(arr.size() < k){
             return -1;
         }
         return Binary(arr , k);
