52905c46bbc096321846e8628a811406266d7207
Harsh Saini <harsh.saini13881@gmail.com>
2026-08-17 19:39:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check Perfect Square/check-perfect-square.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    bool isPerfectSquare(int n) {
+        for(int i = 0 ; i <= (n/2)+1 ; i++){
+            if(i*i == n){
+                return true;
+            }
+        }
+        return false;
+    }
+};
