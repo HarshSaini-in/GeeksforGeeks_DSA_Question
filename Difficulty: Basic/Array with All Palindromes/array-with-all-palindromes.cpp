b326ada3593d2f3b303fc9eae97ab53f6ef4ae02
Harsh Saini <harsh.saini13881@gmail.com>
2026-09-14 10:16:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array with All Palindromes/array-with-all-palindromes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    bool Solve(int n){
+        int temp = n;
+        int ans = 0;
+        while(n > 0){
+            int digit = n % 10;
+            ans = ans * 10 + digit;
+            n = n/10;
+        }
+        if(temp == ans){
+            return true;
+        }
+        return false;
+        
+    }
+    bool isPalinArray(vector<int> &arr) {
+        int i = 0;
+        int n = arr.size();
+        while(i < n){
+            if(!Solve(arr[i])){
+                return false;
+            }
+            else{
+                i++;
+            }
+        }
+        return true;
+    }
+};
